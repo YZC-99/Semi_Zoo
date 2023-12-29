d2d6906af11e6a1753ac9c544d3792882279fe63
yzc <yzc@qq.com>
2023-12-29 10:03:24 +0800

    修复bug

--- utils/util.py
+++ utils/util.py
@@ -11,7 +11,7 @@ from scipy.ndimage import distance_transform_edt as distance
 from skimage import segmentation as skimage_seg
 import torch
 from torch.utils.data.sampler import Sampler
-from torch.optim.lr_scheduler import _LRScheduler,LambdaLR,PolynomialLR
+from torch.optim.lr_scheduler import _LRScheduler,LambdaLR
 import torch.optim as optim
 
 
