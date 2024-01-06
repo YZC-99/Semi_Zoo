7f39b2e2977060b94c1cd311cba95d99c3886f2e
yzc <yzc@qq.com>
2024-01-06 20:44:45 +0800

    添加了Dual_Seg_Head_Unet

--- model/netwotks/dual_decoer_unet.py
+++ model/netwotks/dual_decoer_unet.py
@@ -91,3 +91,79 @@ class Dual_Decoder_Unet(SegmentationModel):
             return masks, labels
 
         return masks,masks2
+
+
+
+class Dual_Seg_Head_Unet(SegmentationModel):
+
+
+    def __init__(
+        self,
+        encoder_name: str = "resnet34",
+        encoder_depth: int = 5,
+        encoder_weights: Optional[str] = "imagenet",
+        decoder_use_batchnorm: bool = True,
+        decoder_channels: List[int] = (256, 128, 64, 32, 16),
+        decoder_attention_type: Optional[str] = None,
+        in_channels: int = 3,
+        classes: int = 1,
+        activation: Optional[Union[str, callable]] = None,
+        aux_params: Optional[dict] = None,
+    ):
+        super().__init__()
+
+        self.encoder = get_encoder(
+            encoder_name,
+            in_channels=in_channels,
+            depth=encoder_depth,
+            weights=encoder_weights,
+        )
+
+        self.decoder = UnetDecoder(
+            encoder_channels=self.encoder.out_channels,
+            decoder_channels=decoder_channels,
+            n_blocks=encoder_depth,
+            use_batchnorm=decoder_use_batchnorm,
+            center=True if encoder_name.startswith("vgg") else False,
+            attention_type=decoder_attention_type,
+        )
+
+        self.segmentation_head = SegmentationHead(
+            in_channels=decoder_channels[-1],
+            out_channels=classes,
+            activation=activation,
+            kernel_size=3,
+        )
+
+
+        self.segmentation_head2 = SegmentationHead(
+            in_channels=decoder_channels[-1],
+            out_channels=2,
+            activation=activation,
+            kernel_size=3,
+        )
+
+        if aux_params is not None:
+            self.classification_head = ClassificationHead(in_channels=self.encoder.out_channels[-1], **aux_params)
+        else:
+            self.classification_head = None
+
+        self.name = "u-{}".format(encoder_name)
+        self.initialize()
+
+    def forward(self, x):
+        """Sequentially pass `x` trough model`s encoder, decoder and heads"""
+
+        self.check_input_shape(x)
+
+        features = self.encoder(x)
+        decoder_output = self.decoder(*features)
+
+        masks = self.segmentation_head(decoder_output)
+        masks2 = self.segmentation_head2(decoder_output)
+
+        if self.classification_head is not None:
+            labels = self.classification_head(features[-1])
+            return masks, labels
+
+        return masks,masks2
--- train_idrid_supervised_2d_aux.py
+++ train_idrid_supervised_2d_aux.py
@@ -16,7 +16,7 @@ import torch.nn.functional as F
 from tensorboardX import SummaryWriter
 import segmentation_models_pytorch as smp
 from utils import ramps,losses
-from model.netwotks.dual_decoer_unet import Dual_Decoder_Unet
+from model.netwotks.dual_decoer_unet import Dual_Decoder_Unet,Dual_Seg_Head_Unet
 from utils.losses import OhemCrossEntropy,annealing_softmax_focalloss,softmax_focalloss,weight_softmax_focalloss
 from utils.test_utils import DR_metrics,Sklearn_DR_metrics
 from utils.util import color_map,gray_to_color
@@ -113,6 +113,15 @@ def build_model(model,backbone,in_chns,class_num1,class_num2,fuse_type,ckpt_weig
             in_channels=in_chns,
             classes=class_num1,
         )
+    elif model == 'Dual_Seg_Head_Unet':
+        net = Dual_Seg_Head_Unet(
+            encoder_name=backbone,
+            encoder_weights='imagenet',
+            in_channels=in_chns,
+            classes=class_num1,
+        )
+
+
 
     return net
 
