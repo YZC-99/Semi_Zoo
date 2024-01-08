f1f1a52b6382cfbc744ffb47f4352ceda147a747
yzc <yzc@qq.com>
2024-01-08 13:16:27 +0800

    优化了训练代码

--- run.sh
+++ run.sh
@@ -1,38 +1,36 @@
 python train_idrid_supervised_2d_smp.py \
     --num_works 8        \
     --device 0         \
-    --exp crop_IDRID_smp_LightNet_wFPN-pre_wSR-pre_ex/resnet50_shuffle/imgz1440_bs2_Adam_CLAHE2_warmup0_newschduler_max_iterations-5000_polyv2_lr3e-4         \
+    --exp crop_IDRID_smp_LightNet_wFPN_wSR_ex/resnet50_shuffle/imgz1024_bs4_SGD_CLAHE2_warmup0_newschduler_max_iterations-5000_polyv2_lr7e-4        \
     --dataset_name crop_IDRID         \
     --val_period 54         \
-    --image_size 1440         \
+    --image_size 1024         \
     --model LightNet_wFPN_wSR         \
-    --optim Adam         \
-    --batch_size 2         \
+    --optim SGD         \
+    --batch_size 4         \
     --warmup 0.0         \
-    --base_lr 0.0003         \
+    --base_lr 0.007         \
     --CLAHE 2         \
     --ce_weight 1 1 1 1 1         \
     --max_iterations 5000         \
     --autodl         \
     --fpn_out_c 256 \
     --sr_out_c 256 \
-    --sr_pretrained \
-    --fpn_pretrained \
     --backbone resnet50
 
 
 python train_idrid_supervised_2d_smp.py \
     --num_works 8        \
     --device 0         \
-    --exp crop_IDRID_smp_SR_Unet_ex/resnet50_shuffle/imgz1024_bs2_Adam_CLAHE2_warmup0_newschduler_max_iterations-5000_polyv2_lr3e-4         \
+    --exp crop_IDRID_smp_SR_Unet_ex/resnet50_shuffle/imgz1024_bs4_SGD_CLAHE2_warmup0_newschduler_max_iterations-5000_polyv2_lr7e-3         \
     --dataset_name crop_IDRID         \
     --val_period 54         \
     --image_size 1024         \
     --model SR_Unet         \
-    --optim Adam         \
-    --batch_size 2         \
+    --optim SGD         \
+    --batch_size 4         \
     --warmup 0.0         \
-    --base_lr 0.0003         \
+    --base_lr 0.007         \
     --CLAHE 2         \
     --ce_weight 1 1 1 1 1         \
     --max_iterations 5000         \
