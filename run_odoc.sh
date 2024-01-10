05bc08da47e5efce65dd88890d74d272e83c0fd2
yzc <yzc@qq.com>
2024-01-10 14:41:05 +0800

    添加了血管辅助分割任务的代码

--- run_odoc.sh
+++ run_odoc.sh
@@ -16,10 +16,10 @@ python train_odoc_supervised_2d_smp.py \
 python train_odoc_supervised_2d_smp_aux.py \
     --num_works 8        \
     --device 0         \
-    --exp RIM-ONE-Dual_Seg_Head_Unet-aux/resnet50/imgz512_bs8_Adam_warmup0_iterations-4000_polyv2_lr3e-4        \
+    --exp RIM-ONE-Dual_Decoder_Unet-aux/resnet50/imgz512_bs8_Adam_warmup0_iterations-4000_polyv2_lr3e-4        \
     --dataset_name RIM-ONE         \
     --image_size 512         \
-    --model Dual_Seg_Head_Unet         \
+    --model Dual_Decoder_Unet         \
     --optim Adam         \
     --batch_size 8         \
     --labeled_bs 4         \
