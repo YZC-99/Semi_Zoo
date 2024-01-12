a78b82814a00447025e2f28ccb95bf5f5d2699f6
yzc <yzc@qq.com>
2024-01-12 13:01:57 +0800

    fixbug

--- vessel_refer.py
+++ vessel_refer.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
 
     labeled_dataset = SemiDataset(name='./dataset/{}'.format(args.dataset_name),
                                   root="{}{}".format(root_base, args.dataset_name),
-                                  mode='val',
+                                  mode='test',
                                   size=args.image_size,
                                   id_path='train.txt')
 
