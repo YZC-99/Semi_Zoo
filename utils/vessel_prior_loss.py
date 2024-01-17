aa768abf24c6bae01ea7f366c112a573314323d5
yzc <yzc@qq.com>
2024-01-17 15:24:42 +0800

    fixing Kink-Loss bugs

--- utils/vessel_prior_loss.py
+++ utils/vessel_prior_loss.py
@@ -26,15 +26,19 @@ class KinkLoss(nn.Module):
         # 获取 kink_mask 中值为 1 的位置的索引
         kink_indices = torch.nonzero(kink_mask == 1, as_tuple=False)
 
+        odoc_mask_at_kink = torch.zeros_like(odoc_mask)
+
         # 从 features 和 odoc_mask 中提取对应索引位置的值
         features_at_kink = features[kink_indices[:, 0], :, kink_indices[:, 1], kink_indices[:, 2]]
-        odoc_mask_at_kink = odoc_mask[kink_indices[:, 0], kink_indices[:, 1], kink_indices[:, 2]]
+        # odoc_mask_at_kink = odoc_mask[kink_indices[:, 0], kink_indices[:, 1], kink_indices[:, 2]]
+        odoc_mask_at_kink[kink_mask == 1] = 1
 
         oc_features_center = self.oc_features_center(features, odoc_mask)
         oc_features_center = oc_features_center.tile((features_at_kink.shape[0],1))
 
         simi = F.cosine_similarity(oc_features_center, features_at_kink)
 
+        odoc_mask_at_kink = torch.reshape(odoc_mask_at_kink,(simi.shape[0],-1))
         # 计算交叉熵损失
         # mse_loss = F.mse_loss(oc_features_center.detach(), features_at_kink)
         mse_loss = F.mse_loss(simi, odoc_mask_at_kink.float())
