72b73160892198b911519af8b8e568ba763fb26e
yzc <yzc@qq.com>
2024-01-23 12:23:49 +0800

    add Unet_wRTFM_wFPN

--- scripts/231231.sh
+++ scripts/231231.sh
@@ -1,10 +1,10 @@
 python train_idrid_supervised_2d_smp.py \
         --num_works 4 \
         --device 0 \
-        --exp crop_IDRID/Unet_wFPN/resnet50/imgz1024_bs2_CLAHE2_lr2e-4_5k \
+        --exp crop_IDRID/Unet_wRTFM_wFPN/resnet50/imgz1024_bs2_CLAHE2_lr2e-4_5k \
         --dataset_name crop_IDRID \
         --image_size 1024 \
-        --model Unet_wFPN \
+        --model Unet_wRTFM_wFPN \
         --optim Adam \
         --batch_size 2 \
         --base_lr 0.0002 \
@@ -14,5 +14,7 @@ python train_idrid_supervised_2d_smp.py \
         --backbone resnet50
 
 Unet_wRTFM
+Unet_wRTFM_wFPN
+Unet_wFPN
 Unet_wRTFM_wFPN_wlightDecoder
 Unet_wFPN_wlightDecoder
