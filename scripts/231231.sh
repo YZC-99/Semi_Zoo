48583ff1461fb83c2f5b34ef4bb23112f9ed244f
yzc <yzc@qq.com>
2024-01-25 16:50:18 +0800

    add model

--- scripts/231231.sh
+++ scripts/231231.sh
@@ -1,23 +1,41 @@
 python train_idrid_supervised_2d_smp.py \
         --num_works 4 \
         --device 0 \
-        --exp crop_IDRID/Dual_Decoder_Unet_wFPN_wDAB/resnet50/nolrdecay/imgz1024_bs2_Adam_CLAHE0_lr3e-4_5k\
+        --exp crop_IDRID/Unet_wFPN_wDAB/resnet50/imgz1024_bs2_Adam_CLAHE0_lr3e-4-2k\
         --dataset_name crop_IDRID \
         --image_size 1024 \
-        --model Dual_Decoder_Unet_wFPN_wDAB \
+        --model Unet_wFPN_wDAB \
         --optim Adam \
         --batch_size 2 \
         --base_lr 0.0003 \
         --CLAHE 0 \
         --autodl \
         --scheduler no\
-        --max_iterations 5000 \
-        --obj_loss 1.0 \
+        --max_iterations 2000 \
         --backbone resnet50
 
-        --main_criteria softmax_focal \
+
+python train_idrid_supervised_2d_smp.py \
+        --num_works 4 \
+        --device 0 \
+        --exp crop_IDRID/find_param/Unet_wFPN_wDAB/resnet50/imgz1024_bs2_Adam_CLAHE0_lr1e-2k\
+        --dataset_name crop_IDRID \
+        --image_size 1024 \
+        --model Unet_wFPN_wDAB \
+        --optim Adam \
+        --batch_size 2 \
+        --base_lr 0.001 \
+        --CLAHE 0 \
+        --autodl \
+        --scheduler no\
+        --max_iterations 2000 \
+        --backbone resnet50
 
         --obj_loss 1.0 \
+                --main_criteria softmax_focal \
+
+
+
 
 DeepLabv3p
 UNetpp
@@ -31,6 +49,7 @@ Unet_wFPN_wSR
 Unet_wRTFM_wFPN_wlightDecoder
 Unet_wFPN_wlightDecoder
 Dual_Decoder_Unet
+Dual_Decoder_Unet_wFPN_wDAB
 Dual_Seg_Head_Unet
   mobileone_s3
   efficientnet-b0
