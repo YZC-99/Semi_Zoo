fbaf83284947a16029aa1a48364a3420ac54fbd1
yzc <yzc@qq.com>
2024-01-26 14:19:42 +0800

    fixed bugs

--- model/module/attentions.py
+++ model/module/attentions.py
@@ -4,66 +4,6 @@ from torch.nn import Softmax
 from torch.nn import init
 
 
-class PSA(nn.Module):
-
-    def __init__(self, channel=512, reduction=4, S=4):
-        super().__init__()
-        self.S = S
-
-        self.convs = []
-        for i in range(S):
-            self.convs.append(nn.Conv2d(channel // S, channel // S, kernel_size=2 * (i + 1) + 1, padding=i + 1))
-
-        self.se_blocks = []
-        for i in range(S):
-            self.se_blocks.append(nn.Sequential(
-                nn.AdaptiveAvgPool2d(1),
-                nn.Conv2d(channel // S, channel // (S * reduction), kernel_size=1, bias=False),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(channel // (S * reduction), channel // S, kernel_size=1, bias=False),
-                nn.Sigmoid()
-            ))
-
-        self.softmax = nn.Softmax(dim=1)
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        b, c, h, w = x.size()
-
-        # Step1:SPC module
-        SPC_out = x.view(b, self.S, c // self.S, h, w)  # bs,s,ci,h,w
-        for idx, conv in enumerate(self.convs):
-            SPC_out[:, idx, :, :, :] = conv(SPC_out[:, idx, :, :, :])
-
-        # Step2:SE weight
-        se_out = []
-        for idx, se in enumerate(self.se_blocks):
-            se_out.append(se(SPC_out[:, idx, :, :, :]))
-        SE_out = torch.stack(se_out, dim=1)
-        SE_out = SE_out.expand_as(SPC_out)
-
-        # Step3:Softmax
-        softmax_out = self.softmax(SE_out)
-
-        # Step4:SPA
-        PSA_out = SPC_out * softmax_out
-        PSA_out = PSA_out.view(b, -1, h, w)
-
-        return PSA_out
-
 
 class SCSEModule(nn.Module):
     def __init__(self, in_channels, reduction=16):
@@ -92,7 +32,7 @@ class SCPSAMModule(nn.Module):
             nn.Sigmoid(),
         )
         self.sSE = nn.Sequential(nn.Conv2d(in_channels, 1, 1), nn.Sigmoid())
-        self.psa = PSA(in_channels,reduction=reduction)
+        self.psa = PSAModule(in_channels,reduction=reduction)
 
     def forward(self, x):
         self.psa.to(x.device)
@@ -109,7 +49,7 @@ class SC2PSAMModule(nn.Module):
             nn.Sigmoid(),
         )
         self.sSE = nn.Sequential(nn.Conv2d(in_channels, 1, 1), nn.Sigmoid())
-        self.psa = PSA(in_channels,reduction=reduction)
+        self.psa = PSAModule(in_channels,reduction=reduction)
 
     def forward(self, x):
         scse_out = x * self.cSE(x) + x * self.sSE(x)
