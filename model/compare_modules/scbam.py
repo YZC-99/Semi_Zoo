d7b09023ab77f1a9ed9d5f9743ef32996a902a94
yzc <yzc@qq.com>
2024-01-27 12:56:36 +0800

    add Unet_wFPN_wSCBAM

--- model/compare_modules/scbam.py
+++ model/compare_modules/scbam.py
@@ -2,8 +2,26 @@ import torch
 import torch.nn as nn
 from torch.nn import Softmax
 from torch.nn import init
-from model.module.attentions import SCCBAMMModule
+from model.module.attentions import SCBAMMModule,SCCBAMMModule
 
+
+class SCBAM_Module(nn.Module):
+
+    def __init__(self, in_channels_list):
+        super(SCBAM_Module, self).__init__()
+
+        self.sa_layers = []
+        for idx,in_channels in enumerate(in_channels_list,1):
+            sa_layer = 'ska_layer{}'.format(idx)
+            sa_layer_module = SCBAMMModule( in_channels)
+            self.add_module(sa_layer,sa_layer_module)
+            self.sa_layers.append(sa_layer)
+
+    def forward(self, fpn_features):
+        for idx,(feature,sa) in enumerate(zip(fpn_features,self.sa_layers)):
+            feature_out = getattr(self,sa)(feature)
+            fpn_features[idx] = feature_out
+        return fpn_features
 
 class SCCBAM_Module(nn.Module):
 
--- scripts/231231.sh
+++ scripts/231231.sh
@@ -18,7 +18,7 @@
 python train_idrid_supervised_2d_smp.py \
         --num_works 4 \
         --device 0 \
-        --exp crop_IDRID/Unet_wFPN_wSCBAM/se_resnet50/imgz1024_bs2_Adam_CLAHE0_lr3e-4-5k\
+        --exp crop_IDRID/Unet_wFPN_wSCBAM-true/se_resnet50/imgz1024_bs2_Adam_CLAHE0_lr3e-4-5k\
         --dataset_name crop_IDRID \
         --image_size 1024 \
         --model Unet_wFPN_wSCBAM \
