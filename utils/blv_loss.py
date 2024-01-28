567f1846a02889d51306c7ccc497701fe14640f7
yzc <yzc@qq.com>
2024-01-28 15:08:18 +0800

    add blv-loss

--- utils/blv_loss.py
+++ utils/blv_loss.py
@@ -2,21 +2,112 @@ import torch
 import torch.nn as nn
 from torch.distributions import normal
 import torch.nn.functional as F
+import mmcv
+import numpy as np
+
+
+def get_class_weight(class_weight):
+    """Get class weight for loss function.
+
+    Args:
+        class_weight (list[float] | str | None): If class_weight is a str,
+            take it as a file name and read from it.
+    """
+    if isinstance(class_weight, str):
+        # take it as a file path
+        if class_weight.endswith('.npy'):
+            class_weight = np.load(class_weight)
+        else:
+            # pkl, json or yaml
+            class_weight = mmcv.load(class_weight)
+
+    return class_weight
+
+def reduce_loss(loss, reduction):
+    """Reduce loss as specified.
+
+    Args:
+        loss (Tensor): Elementwise loss tensor.
+        reduction (str): Options are "none", "mean" and "sum".
+
+    Return:
+        Tensor: Reduced loss tensor.
+    """
+    reduction_enum = F._Reduction.get_enum(reduction)
+    # none: 0, elementwise_mean:1, sum: 2
+    if reduction_enum == 0:
+        return loss
+    elif reduction_enum == 1:
+        return loss.mean()
+    elif reduction_enum == 2:
+        return loss.sum()
+
+
+def weight_reduce_loss(loss, weight=None, reduction='mean', avg_factor=None):
+    """Apply element-wise weight and reduce loss.
+
+    Args:
+        loss (Tensor): Element-wise loss.
+        weight (Tensor): Element-wise weights.
+        reduction (str): Same as built-in losses of PyTorch.
+        avg_factor (float): Average factor when computing the mean of losses.
+
+    Returns:
+        Tensor: Processed loss values.
+    """
+    # if weight is specified, apply element-wise weight
+    if weight is not None:
+        assert weight.dim() == loss.dim()
+        if weight.dim() > 1:
+            assert weight.size(1) == 1 or weight.size(1) == loss.size(1)
+        loss = loss * weight
+
+    # if avg_factor is not specified, just reduce the loss
+    if avg_factor is None:
+        loss = reduce_loss(loss, reduction)
+    else:
+        # if reduction is mean, then average the loss by avg_factor
+        if reduction == 'mean':
+            # Avoid causing ZeroDivisionError when avg_factor is 0.0,
+            # i.e., all labels of an image belong to ignore index.
+            eps = torch.finfo(torch.float32).eps
+            loss = loss.sum() / (avg_factor + eps)
+        # if reduction is 'none', then do nothing, otherwise raise an error
+        elif reduction != 'none':
+            raise ValueError('avg_factor can not be used with reduction="sum"')
+    return loss
+
 
 class BlvLoss(nn.Module):
-    def __init__(self, cls_num_list, sigma=4):
+#cls_nufrequency_list
+    def __init__(self, cls_num_list, sigma=4, loss_name='BlvLoss'):
         super(BlvLoss, self).__init__()
-        cls_num_list = torch.tensor(cls_num_list)
-        cls_list = torch.tensor(cls_num_list, dtype=torch.float)
+        cls_list = torch.cuda.FloatTensor(cls_num_list)
         frequency_list = torch.log(cls_list)
         self.frequency_list = torch.log(sum(cls_num_list)) - frequency_list
+        self.reduction = 'mean'
         self.sampler = normal.Normal(0, sigma)
-        self._loss_name = 'BlvLoss'
+        self._loss_name = loss_name
 
-    def forward(self, pred, target):
-        viariation = self.sampler.sample(pred.shape).clamp(-1, 1)
-        viariation = viariation.to(pred.device)
-        self.frequency_list = self.frequency_list.to(pred.device)
+
+
+    def forward(self, pred, target, weight=None, ignore_index=None, avg_factor=None, reduction_override=None):
+
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+
+        viariation = self.sampler.sample(pred.shape).clamp(-1, 1).to(pred.device)
+
         pred = pred + (viariation.abs().permute(0, 2, 3, 1) / self.frequency_list.max() * self.frequency_list).permute(0, 3, 1, 2)
-        loss = F.cross_entropy(pred, target, reduction='none')
+
+        loss = F.cross_entropy(pred, target, reduction='none',  ignore_index=ignore_index)
+
+        if weight is not None:
+            weight = weight.float()
+
+        loss = weight_reduce_loss(
+            loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+
         return loss
+
