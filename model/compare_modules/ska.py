1ccd86f5c27da0c09da68eb5736e443d7a640663
yzc <yzc@qq.com>
2024-02-05 16:06:57 +0800

    add Unet_wFPN_wSKA_Dali

--- model/compare_modules/ska.py
+++ model/compare_modules/ska.py
@@ -105,7 +105,7 @@ class SK_Dali_Attention(nn.Module):
         self.convs.append(DAM(channel))
         self.fc=nn.Linear(channel,self.d)
         self.fcs=nn.ModuleList([])
-        for i in range(len(kernels)):
+        for i in range(len(kernels)+1):
             self.fcs.append(nn.Linear(self.d,channel))
         self.softmax=nn.Softmax(dim=0)
 
