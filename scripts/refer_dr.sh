13e34b85146946d01cc8dbbf039d0cb98abe968f
yzc <yzc@qq.com>
2024-02-29 20:28:46 +0800

    add Unet_wDeocderAttention

--- scripts/refer_dr.sh
+++ scripts/refer_dr.sh
@@ -2,16 +2,16 @@
 python test_idrid_supervised_2d_smp.py \
         --num_works 8 \
         --device 0 \
-        --exp crop_IDRID/Unet_wFPN_wDeocderAttention-softmax_focal_blv \
+        --exp crop_IDRID/Unet_wFPN \
         --dataset_name crop_IDRID \
         --image_size 1440 \
-        --model Unet_wFPN_wDeocderAttention \
-        --decoder_attention_type scse \
+        --model Unet_wFPN \
         --CLAHE 0 \
         --autodl \
-        --ckpt_weight /root/autodl-tmp/Semi_Zoo/exp_2d_dr/crop_IDRID/Unet_wFPN_wDeocderAttention-softmax_focal_blv/ema-lr6e-4_poly-v2-interval-30/imgz1440_se_resnet50/bs2_Adam_CLAHE0-5k/version0/best_AUC_PR_EX_0.813_iter_3672.pth \
+        --ckpt_weight /root/autodl-tmp/Semi_Zoo/exp_2d_dr/crop_IDRID/Unet_wFPN/ema-lr6e-4_poly-v2-interval-30/imgz1440_se_resnet50/bs2_Adam_CLAHE0-5k/version0/best_AUC_PR_EX0.8106_iter_4698.pth \
         --backbone se_resnet50
 
+        --decoder_attention_type scse \
 
 Unet_wFPN_wSKA
 Unet_wFPN
--- test_idrid_supervised_2d_smp.py
+++ test_idrid_supervised_2d_smp.py
@@ -1,18 +1,19 @@
 import random
 from tqdm import tqdm
-from dataloader.fundus import SemiDataset,IDRIDDataset
+from dataloader.fundus import SemiDataset, IDRIDDataset
 import torch
 import glob
 import argparse
-from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss,MSELoss
+import matplotlib.pyplot as plt
+from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss, MSELoss
 from torch.utils.data import DataLoader, WeightedRandomSampler
 from tensorboardX import SummaryWriter
 import segmentation_models_pytorch as smp
-from utils.losses import OhemCrossEntropy,annealing_softmax_focalloss,softmax_focalloss,weight_softmax_focalloss
-from utils.test_utils import DR_metrics,Sklearn_DR_metrics
-from utils.util import color_map,gray_to_color
+from utils.losses import OhemCrossEntropy, annealing_softmax_focalloss, softmax_focalloss, weight_softmax_focalloss
+from utils.test_utils import DR_metrics, Sklearn_DR_metrics
+from utils.util import color_map, gray_to_color
 import random
-from utils.util import get_optimizer,PolyLRwithWarmup, compute_sdf,compute_sdf_luoxd,compute_sdf_multi_class
+from utils.util import get_optimizer, PolyLRwithWarmup, compute_sdf, compute_sdf_luoxd, compute_sdf_multi_class
 from utils.scheduler.my_scheduler import my_decay_v1
 from utils.bulid_model import build_model
 from PIL import Image
@@ -29,63 +30,69 @@ from torchvision import transforms
 import torchvision.transforms.functional as F
 import sys
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
-from utils.scheduler.poly_lr import poly_epoch_decay,poly_epoch_decay_v2,poly_epoch_decay_v3
+from utils.scheduler.poly_lr import poly_epoch_decay, poly_epoch_decay_v2, poly_epoch_decay_v3
 import ttach as tta
 
 parser = argparse.ArgumentParser()
 
 # ==============model===================
-parser.add_argument('--model',type=str,default='unet')
-parser.add_argument('--backbone',type=str,default='b2')
-parser.add_argument('--encoder_deepth',type=int,default=5)
-parser.add_argument('--fpn_out_c',type=int,default=-1,help='the out-channels of the FPN module')
-parser.add_argument('--fpn_pretrained',action='store_true')
-parser.add_argument('--sr_out_c',type=int,default=128,help='the out-channels of the SR module')
-parser.add_argument('--sr_pretrained',action='store_true')
-parser.add_argument('--decoder_attention_type',type=str,default=None,choices=['scse','sccbam','sc2cbam','scbam','sc2bam'])
-parser.add_argument('--ckpt_weight',type=str,default=None)
-parser.add_argument('--exclude_keys',type=str,default=None)
+parser.add_argument('--model', type=str, default='unet')
+parser.add_argument('--backbone', type=str, default='b2')
+parser.add_argument('--encoder_deepth', type=int, default=5)
+parser.add_argument('--fpn_out_c', type=int, default=-1, help='the out-channels of the FPN module')
+parser.add_argument('--fpn_pretrained', action='store_true')
+parser.add_argument('--sr_out_c', type=int, default=128, help='the out-channels of the SR module')
+parser.add_argument('--sr_pretrained', action='store_true')
+parser.add_argument('--decoder_attention_type', type=str, default=None,
+                    choices=['scse', 'sccbam', 'sc2cbam', 'scbam', 'sc2bam'])
+parser.add_argument('--ckpt_weight', type=str, default=None)
+parser.add_argument('--exclude_keys', type=str, default=None)
 # ==============model===================
 
 # ==============loss===================
-parser.add_argument('--main_criteria', type=str, default='ce',choices=['ce','weight-ce','dice','ce-dice','blv','softmax_focal_blv','softmax_focal','annealing_softmax_focal'])
+parser.add_argument('--main_criteria', type=str, default='ce',
+                    choices=['ce', 'weight-ce', 'dice', 'ce-dice', 'blv', 'softmax_focal_blv', 'softmax_focal',
+                             'annealing_softmax_focal'])
 parser.add_argument('--obj_loss', type=float, default=-1.0)
-parser.add_argument('--ce_weight', type=float, nargs='+', default=[1,1,1,1,1], help='List of floating-point values')
-parser.add_argument('--cls_num_list', type=float, nargs='+', default=[645688315,704963,6498614,5319349,1248855], help='')
-parser.add_argument('--ohem',type=float,default=-1.0)
-parser.add_argument('--annealing_softmax_focalloss',action='store_true')
-parser.add_argument('--softmax_focalloss',action='store_true')
-parser.add_argument('--weight_softmax_focalloss',action='store_true')
-parser.add_argument('--with_dice',action='store_true')
+parser.add_argument('--ce_weight', type=float, nargs='+', default=[1, 1, 1, 1, 1], help='List of floating-point values')
+parser.add_argument('--cls_num_list', type=float, nargs='+', default=[645688315, 704963, 6498614, 5319349, 1248855],
+                    help='')
+parser.add_argument('--ohem', type=float, default=-1.0)
+parser.add_argument('--annealing_softmax_focalloss', action='store_true')
+parser.add_argument('--softmax_focalloss', action='store_true')
+parser.add_argument('--weight_softmax_focalloss', action='store_true')
+parser.add_argument('--with_dice', action='store_true')
 # ==============loss===================
 
 # ==============lr===================
-parser.add_argument('--base_lr',type=float,default=0.00025)
-parser.add_argument('--lr_decouple',action='store_true')
-parser.add_argument('--warmup',type=float,default=0.01)
-parser.add_argument('--scheduler',type=str,default='poly-v1',choices=['poly-v1','poly-v2','poly-v3','poly','no','my_decay_v1'])
+parser.add_argument('--base_lr', type=float, default=0.00025)
+parser.add_argument('--lr_decouple', action='store_true')
+parser.add_argument('--warmup', type=float, default=0.01)
+parser.add_argument('--scheduler', type=str, default='poly-v1',
+                    choices=['poly-v1', 'poly-v2', 'poly-v3', 'poly', 'no', 'my_decay_v1'])
 # ==============lr===================
 
 # ==============training params===================
-parser.add_argument('--seed',type=int,default=42)
-parser.add_argument('--device',type=int,default=0)
-parser.add_argument('--num_works',type=int,default=0)
-parser.add_argument('--num_classes',type=int,default=5)
-parser.add_argument('--exp',type=str,default='IDRID')
-parser.add_argument('--save_period',type=int,default=5000)
-parser.add_argument('--val_period',type=int,default=100)
-parser.add_argument('--dataset_name',type=str,default='IDRID')
-parser.add_argument('--CLAHE',type=int,default=2)
-parser.add_argument('--optim',type=str,default='AdamW')
-parser.add_argument('--batch_size',type=int,default=4)
-parser.add_argument('--image_size',type=int,default=512)
-parser.add_argument('--max_iterations',type=int,default=10000)
-parser.add_argument('--autodl',action='store_true')
-parser.add_argument('--cutmix_prob',default=-1.0)
+parser.add_argument('--seed', type=int, default=42)
+parser.add_argument('--device', type=int, default=0)
+parser.add_argument('--num_works', type=int, default=0)
+parser.add_argument('--num_classes', type=int, default=5)
+parser.add_argument('--exp', type=str, default='IDRID')
+parser.add_argument('--save_period', type=int, default=5000)
+parser.add_argument('--val_period', type=int, default=100)
+parser.add_argument('--dataset_name', type=str, default='IDRID')
+parser.add_argument('--CLAHE', type=int, default=2)
+parser.add_argument('--optim', type=str, default='AdamW')
+parser.add_argument('--batch_size', type=int, default=4)
+parser.add_argument('--image_size', type=int, default=512)
+parser.add_argument('--max_iterations', type=int, default=10000)
+parser.add_argument('--autodl', action='store_true')
+parser.add_argument('--cutmix_prob', default=-1.0)
 
 # ==============test params===================
-parser.add_argument('--tta',action='store_true')
+parser.add_argument('--tta', action='store_true')
 
 
 def color_map_fn():
@@ -97,9 +104,11 @@ def color_map_fn():
     cmap[4] = np.array([255, 255, 0])
     return cmap
 
+
 # 定义一个函数来执行旋转，接受图像和角度作为输入
 def rotate_image(image, angle):
     return F.rotate(image, angle)
+
 
 tta.aliases.d4_transform()
 
@@ -119,6 +128,7 @@ def tta_transforms(image):
     inverse_transforms = [inv_t for _, inv_t in transforms]
 
     return augmented_images, inverse_transforms
+
 
 def apply_tta_and_predict(model, image_tensor, device):
     augmented_images, inverse_transforms = tta_transforms(image_tensor)
@@ -167,7 +177,6 @@ snapshot_path = "./exp_refer_2d_dr/" + args.exp + "/"
 max_iterations = args.max_iterations
 base_lr = args.base_lr
 
-
 if __name__ == '__main__':
 
     if not os.path.exists(snapshot_path):
@@ -181,54 +190,83 @@ if __name__ == '__main__':
     device = "cuda:{}".format(args.device)
 
     # init model
-    model = build_model(args,model=args.model,backbone=args.backbone,in_chns=3,class_num1=args.num_classes,class_num2=2,fuse_type=None,ckpt_weight=args.ckpt_weight)
+    model = build_model(args, model=args.model, backbone=args.backbone, in_chns=3, class_num1=args.num_classes,
+                        class_num2=2, fuse_type=None, ckpt_weight=args.ckpt_weight)
     model.to(device)
-
 
     # init dataset
     root_base = '/home/gu721/yzc/data/dr/'
     if args.autodl:
         root_base = '/root/autodl-tmp/'
 
-
     # 验证集
     # init dataset
     val_dataset = IDRIDDataset(name='./dataset/{}'.format(args.dataset_name),
-                                    # root="D:/1-Study/220803研究生阶段学习/221216论文写作专区/OD_OC/数据集/REFUGE",
-                                  root="{}{}".format(root_base,args.dataset_name),
-                                  mode='val',
-                                  size=args.image_size,
-                                  CLAHE=args.CLAHE)
+                               # root="D:/1-Study/220803研究生阶段学习/221216论文写作专区/OD_OC/数据集/REFUGE",
+                               root="{}{}".format(root_base, args.dataset_name),
+                               mode='val',
+                               size=args.image_size,
+                               CLAHE=args.CLAHE)
 
-    val_loader = DataLoader(val_dataset,batch_size=1,num_workers=1)
+    val_loader = DataLoader(val_dataset, batch_size=1, num_workers=1)
     val_iteriter = tqdm(val_loader)
 
     model.eval()
     DR_val_metrics = DR_metrics(device)
 
-    for i_batch,labeled_sampled_batch in enumerate(val_loader):
+    for i_batch, labeled_sampled_batch in enumerate(val_loader):
         time2 = time.time()
-        labeled_batch, label_label_batch = labeled_sampled_batch['image'].to(device), labeled_sampled_batch['label'].to(device)
+        labeled_batch, label_label_batch = labeled_sampled_batch['image'].to(device), labeled_sampled_batch['label'].to(
+            device)
         image_id = labeled_sampled_batch['id'][0].split('/')[-1]
 
         if args.tta:
             outputs = apply_tta_and_predict(model, labeled_batch, device)
         else:
             outputs = model(labeled_batch)
-        out = torch.argmax(outputs,dim=1)
-        out = out[0].detach().cpu().numpy().astype(np.int8)
-        pred_mask = Image.fromarray(out,mode='P')
-        pred_mask.putpalette(color_map_fn())
-        pred_mask.save(results_path+'/'+image_id)
+        out = torch.argmax(outputs, dim=1)
+        class_3_mask = (out == 3)
+        # 创建一个形状与outputs相同、但所有值都设为0的张量
+        class_3_output = torch.zeros_like(out, dtype=outputs.dtype)
 
-        DR_val_metrics.add(outputs.detach(),label_label_batch)
-    with open(results_logs_path,'w') as f:
+        # 注意outputs的形状为[1, 5, h, w]，我们需要在第二维度选取索引为3的通道
+        class_3_values = outputs[:, 3, :, :]
+
+        # 使用torch.where根据class_3_mask将满足条件的位置更新为class_3_values对应的值
+        class_3_output = torch.where(class_3_mask, class_3_values, class_3_output)
+
+        # 确保数据在0到1之间，如果已经在这个范围内，这一步可以跳过
+        class_3_output_normalized = (class_3_output - class_3_output.min()) / (
+                    class_3_output.max() - class_3_output.min())
+
+        # 使用matplotlib绘制热力图
+        plt.imshow(class_3_output_normalized.detach().cpu().numpy().squeeze(), cmap='hot', interpolation='nearest')
+        plt.axis('off')  # 关闭坐标轴
+        plt.tight_layout(pad=0)  # 紧凑布局，不留额外边缘空间
+
+        # 保存图像
+        plt.savefig(results_path + '/hot_' + image_id, bbox_inches='tight', pad_inches=0)
+
+        # 关闭图形，释放资源
+        plt.close()
+        #         print(labeled_batch.shape)
+        #         print(class_3_output.shape)
+
+        out = out[0].detach().cpu().numpy().astype(np.int8)
+        pred_mask = Image.fromarray(out, mode='P')
+        pred_mask.putpalette(color_map_fn())
+        pred_mask.save(results_path + '/' + image_id)
+
+        DR_val_metrics.add(outputs.detach(), label_label_batch)
+    with open(results_logs_path, 'w') as f:
         val_metrics = DR_val_metrics.get_metrics()
         AUC_PR = val_metrics[0]
         AUC_ROC = val_metrics[1]
-        Dice,IoU = val_metrics[2]['Dice'],val_metrics[2]['IoU']
-        MA_AUC_PR, HE_AUC_PR, EX_AUC_PR, SE_AUC_PR = AUC_PR['MA_AUC_PR'],AUC_PR['HE_AUC_PR'],AUC_PR['EX_AUC_PR'],AUC_PR['SE_AUC_PR']
-        MA_AUC_ROC, HE_AUC_ROC, EX_AUC_ROC, SE_AUC_ROC = AUC_ROC['MA_AUC_ROC'],AUC_ROC['HE_AUC_ROC'],AUC_ROC['EX_AUC_ROC'],AUC_ROC['SE_AUC_ROC']
+        Dice, IoU = val_metrics[2]['Dice'], val_metrics[2]['IoU']
+        MA_AUC_PR, HE_AUC_PR, EX_AUC_PR, SE_AUC_PR = AUC_PR['MA_AUC_PR'], AUC_PR['HE_AUC_PR'], AUC_PR['EX_AUC_PR'], \
+                                                     AUC_PR['SE_AUC_PR']
+        MA_AUC_ROC, HE_AUC_ROC, EX_AUC_ROC, SE_AUC_ROC = AUC_ROC['MA_AUC_ROC'], AUC_ROC['HE_AUC_ROC'], AUC_ROC[
+            'EX_AUC_ROC'], AUC_ROC['SE_AUC_ROC']
 
         lines = [
             f'MA_AUC_PR:{MA_AUC_PR}\n',
