d4e44417c9d220534cdc2bd9741bfcaf85c392bb
yzc <yzc@qq.com>
2024-03-12 19:37:16 +0800

    add Unet_wFPN_wPyramidMHSA_SR

--- model/netwotks/sr_unet.py
+++ model/netwotks/sr_unet.py
@@ -208,7 +208,7 @@ class Unet_wFPN_wPyramidMHSA_SR(SegmentationModel):
         new_encoder_channels = list(self.encoder.out_channels)
         new_encoder_channels[-len(self.encoder_fpn_out_channels):] = self.encoder_fpn_out_channels
 
-        self.MHSA = ScaledDotProductAttention(sum(self.encoder.out_channels[-4:]), 128, 128, 12)
+        self.MHSA = ScaledDotProductAttention(sum(self.encoder.out_channels[-4:]), 64, 64, 4)
         self.gap = GlobalAvgPool2D()
 
 
