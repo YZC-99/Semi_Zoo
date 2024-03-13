57ee525efe089cc7e07529368e6e8a15f2e674d0
yzc <yzc@qq.com>
2024-03-13 13:44:07 +0800

    add Unet_wFPN_wDouble_ASPP

--- model/netwotks/sr_unet.py
+++ model/netwotks/sr_unet.py
@@ -683,6 +683,130 @@ class Unet_wFPN_wASPP(SegmentationModel):
         return masks
 
 
+class Unet_wFPN_wDouble_ASPP(SegmentationModel):
+
+    def __init__(
+            self,
+            encoder_name: str = "resnet34",
+            encoder_depth: int = 5,
+            encoder_weights: Optional[str] = "imagenet",
+            fpn_out_channels=256,
+            decoder_use_batchnorm: bool = True,
+            decoder_channels: List[int] = (256, 128, 64, 32, 16),
+            decoder_attention_type: Optional[str] = None,
+            in_channels: int = 3,
+            classes: int = 1,
+            activation: Optional[Union[str, callable]] = None,
+            aux_params: Optional[dict] = None,
+            fpn_pretrained = False,
+    ):
+        super().__init__()
+
+        self.encoder = get_encoder(
+            encoder_name,
+            in_channels=in_channels,
+            depth=encoder_depth,
+            weights=encoder_weights,
+        )
+
+        # --------------
+        # self.zero_layer = False
+        if fpn_out_channels < 0:
+            self.fpn_out_channels = int(sum(self.encoder.out_channels) / len(self.encoder.out_channels))
+        else:
+            self.fpn_out_channels = fpn_out_channels
+
+        fpn_in_channels_list = self.encoder.out_channels
+        fpn_in_channels_list = [i for i in fpn_in_channels_list if i != 0]
+        fpn_in_channels_list = fpn_in_channels_list[-4:]
+        # if len(fpn_in_channels_list) != len(self.encoder.out_channels):
+        #     self.zero_layer = True
+
+        self.fpn = fpn.FPN(
+            in_channels_list=fpn_in_channels_list,
+            out_channels=self.fpn_out_channels,
+            conv_block=fpn.default_conv_block,
+            top_blocks=None, )
+
+        if fpn_pretrained :
+            # Update SceneRelation weights
+            ckpt_apth = 'pretrained/farseg50.pth'
+            sd = torch.load(ckpt_apth)
+            fpn_state_dict = self.fpn.state_dict()
+            for name, param in sd['model'].items():
+
+                if 'module.fpn' in name:
+                    # 移除 'module.' 前缀
+                    name = name.replace('module.', '')
+                    # Update SceneRelation state_dict
+                    fpn_state_dict[name] = param
+            # Load the modified SceneRelation state_dict
+
+            self.fpn.load_state_dict(fpn_state_dict,strict=False)
+            print("================加载FPN权重成功！===============")
+            print(fpn_state_dict.keys())
+            # --------------
+
+
+        self.encoder_fpn_out_channels = [self.fpn_out_channels for i in range(len(fpn_in_channels_list))]
+
+        # --------------
+        # if self.zero_layer:
+        #     self.sr_out_channels.insert(1, 0)
+
+        new_encoder_channels = list(self.encoder.out_channels)
+        new_encoder_channels[-len(self.encoder_fpn_out_channels):] = self.encoder_fpn_out_channels
+        self.decoder = UnetDecoder(
+            encoder_channels=new_encoder_channels,
+            decoder_channels= decoder_channels ,
+            n_blocks=encoder_depth,
+            use_batchnorm=decoder_use_batchnorm,
+            center=True if encoder_name.startswith("vgg") else False,
+            attention_type=decoder_attention_type,
+        )
+
+        self.aspp = ASPP(in_channels=self.encoder.out_channels[-1],out_channels=decoder_channels[-1],atrous_rates=(12, 24, 36))
+        self.aspp2 = ASPP(in_channels=self.fpn_out_channels,out_channels=decoder_channels[-1],atrous_rates=(12, 24, 36))
+
+
+        self.segmentation_head = SegmentationHead(
+            in_channels=decoder_channels[-1] * 3,
+            out_channels=classes,
+            activation=activation,
+            kernel_size=3,
+        )
+
+
+        if aux_params is not None:
+            self.classification_head = ClassificationHead(in_channels=self.encoder.out_channels[-1], **aux_params)
+        else:
+            self.classification_head = None
+
+        self.name = "u-{}".format(encoder_name)
+        self.initialize()
+
+    def forward(self, x):
+        self.check_input_shape(x)
+
+        features = self.encoder(x)
+
+        aspp_feature = self.aspp(features[-1])
+
+        features[-4:] = self.fpn(features[-4:])
+
+        aspp_feature2 = self.aspp2(features[-1])
+
+        decoder_output = self.decoder(*features)
+
+        aspp_feature = F.interpolate(aspp_feature,size=decoder_output.size()[-2:])
+        aspp_feature2 = F.interpolate(aspp_feature2,size=decoder_output.size()[-2:])
+        decoder_output = torch.cat([decoder_output,aspp_feature,aspp_feature2],dim=1)
+
+        masks = self.segmentation_head(decoder_output)
+
+        return masks
+
+
 class Dual_Decoder_Unet_wTri(SegmentationModel):
 
     def __init__(
@@ -1245,7 +1369,7 @@ if __name__ == '__main__':
     # encoder_depth = 4
     # decoder_channels = (256, 128, 64, 32)
 
-    model = Unet_wFPN_wASPP(
+    model = Unet_wFPN_wDouble_ASPP(
         encoder_name=backbone,
         encoder_weights='imagenet',
         in_channels=in_chns,
--- utils/bulid_model.py
+++ utils/bulid_model.py
@@ -8,7 +8,7 @@ from model.netwotks.unet import Unet_wFPN_wASPP_Bottle,Unet_wFPN_wDeocderAttenti
 from model.netwotks.unet import Unet_wSR,Unet_wFPN_wDAB_wSR_wRTFM,Unet_wFPN_wSKA,Unet_wFPN_wSCBAM,Unet_wFPN_wSCCBAM,Unet_wFPN_wSKA_add_Spatial
 from model.netwotks.att_unet import AttU_Net
 from model.netwotks.sr_unet import Unet_wTri,Dual_Decoder_Unet_wTri,Unet_wFPN_wSpatial,Unet_wTri_wLightDecoder
-from model.netwotks.sr_unet import Unet_wFPN_wPyramidMHSA_SR_wLightDecoder,Unet_wFPN_wPyramidMHSA_SR,Unet_wFPN_wASPP
+from model.netwotks.sr_unet import Unet_wFPN_wPyramidMHSA_SR_wLightDecoder,Unet_wFPN_wPyramidMHSA_SR,Unet_wFPN_wASPP,Unet_wFPN_wDouble_ASPP
 from model.netwotks.sr_light_net import LightNet_wFPN,LightNet_wSR,LightNet_wFPN_wSR
 from model.netwotks.dual_decoer_unet import Dual_Decoder_Unet,Dual_Seg_Head_Unet,Dual_Decoder_SR_Unet,Dual_Decoder_SR_Unet_woSR,Dual_Decoder_SR_Unet_woFPN
 from model.netwotks.dual_decoer_unet import Dual_Decoder_Unet_DualFPN_CrossAttention,Dual_Decoder_SR_Unet_woSR_wRTFM
@@ -77,7 +77,16 @@ def build_model(args,model,backbone,in_chns,class_num1,class_num2,fuse_type,ckpt
             encoder_depth=args.encoder_deepth,
             decoder_channels=decoder_channels
         )
-
+    elif model == "Unet_wFPN_wDouble_ASPP":
+        net = Unet_wFPN_wDouble_ASPP(
+            encoder_name=backbone,
+            encoder_weights='imagenet',
+            in_channels=in_chns,
+            classes=class_num1,
+            decoder_attention_type=args.decoder_attention_type,
+            encoder_depth=args.encoder_deepth,
+            decoder_channels=decoder_channels
+        )
 
     elif model == "Unet_wFPN_wDeocderAttention":
         net = Unet_wFPN_wDeocderAttention(
