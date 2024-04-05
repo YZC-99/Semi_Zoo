c3a4940fe0f8996d622fc0d3dcbf003c0fc13ea2
yzc <yzc@qq.com>
2024-04-05 12:36:33 +0800

    add Unet_wASPPv2_wFPN_wDeocderAttention

--- scripts/mlml_extend.sh
+++ scripts/mlml_extend.sh
@@ -1,10 +1,10 @@
 python train_idrid_supervised_2d_smp.py \
         --num_works 8 \
         --device 0 \
-        --exp crop_IDRID/Unet_wASPP_wFPN_wDeocderAttention-softmax_focal_blv/lr6e-4_imgz1440_se_resnet50 \
+        --exp crop_IDRID/Unet_wMSFE_wFPN_wDeocderAttention-softmax_focal_blv/lr6e-4_imgz1440_se_resnet50 \
         --dataset_name crop_IDRID \
         --image_size 1440 \
-        --model Unet_wASPP_wFPN_wDeocderAttention \
+        --model Unet_wMSFE_wFPN_wDeocderAttention \
         --optim Adam \
         --batch_size 2 \
         --base_lr 0.0006 \
@@ -22,3 +22,4 @@ python train_idrid_supervised_2d_smp.py \
 
 Unet_wMSFE_wFPN_wDeocderAttention
 Unet_wASPP_wFPN_wDeocderAttention
+Unet_wASPPv2_wFPN_wDeocderAttention
